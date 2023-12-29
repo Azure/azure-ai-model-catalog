a4634cfc98d8af9b4334394825e64cc40afa5b52
SameepPanigrahi <v-sameepp@microsoft.com>
2023-12-29 14:33:02 +0530

    Checking for next model trigger

--- tests/src/automation_for_constant_library/generic_initial_automation.py
+++ tests/src/automation_for_constant_library/generic_initial_automation.py
@@ -190,15 +190,16 @@ if __name__ == "__main__":
     command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register.py",
                                     environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
     try:
-        create_and_get_job_studio_url(command_job, workspace_ml_client)
-        InferenceAndDeployment = ModelInferenceAndDeployemnt(
-            test_model_name=test_model_name,
-            workspace_ml_client=workspace_ml_client,
-            registry=queue.registry
-        )
-        InferenceAndDeployment.model_infernce_and_deployment(
-            instance_type=queue.instance_type
-        )
+        # create_and_get_job_studio_url(command_job, workspace_ml_client)
+        # InferenceAndDeployment = ModelInferenceAndDeployemnt(
+        #     test_model_name=test_model_name,
+        #     workspace_ml_client=workspace_ml_client,
+        #     registry=queue.registry
+        # )
+        # InferenceAndDeployment.model_infernce_and_deployment(
+        #     instance_type=queue.instance_type
+        # )
+        pass
     except Exception as ex:
         logger.info(f"The failed model is this one: {test_model_name}")
         logger.warning(f"Model Failed due to this {ex}")
