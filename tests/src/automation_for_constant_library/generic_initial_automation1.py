26dc4739b73a10cd8cac9e2d92aac1b7f56946a1
SameepPanigrahi <v-sameepp@microsoft.com>
2024-01-02 15:22:30 +0530

    Refactoring

--- tests/src/automation_for_constant_library/generic_initial_automation1.py
+++ tests/src/automation_for_constant_library/generic_initial_automation1.py
@@ -10,7 +10,10 @@ import os
 import sys
 from box import ConfigBox
 from utils.logging import get_logger
-#from HF_credentials import get_huggingface_token
+from azure.core.exceptions import ResourceNotFoundError
+from azure.ai.ml.entities import ManagedOnlineEndpoint
+
+# from HF_credentials import get_huggingface_token
 huggingface_token = os.environ.get("HF_TOKEN")
 
 # constants
@@ -18,35 +21,37 @@ check_override = True
 
 logger = get_logger(__name__)
 
+
 def get_huggingface_token():
-    return os.environ.get('HF_token', '')
+    return os.environ.get("HF_token", "")
+
 
 def get_error_messages():
     # load ../config/errors.json into a dictionary
-    with open('../../config/errors.json') as f:
+    with open("../../config/errors.json") as f:
         return json.load(f)
 
 
 error_messages = get_error_messages()
 
 # model to test
-test_model_name = os.environ.get('test_model_name')
+test_model_name = os.environ.get("test_model_name")
 
 # test cpu or gpu template
-test_sku_type = os.environ.get('test_sku_type')
+test_sku_type = os.environ.get("test_sku_type")
 
 # bool to decide if we want to trigger the next model in the queue
-test_trigger_next_model = os.environ.get('test_trigger_next_model')
+test_trigger_next_model = os.environ.get("test_trigger_next_model")
 
 # test queue name - the queue file contains the list of models to test with with a specific workspace
-test_queue = os.environ.get('test_queue')
+test_queue = os.environ.get("test_queue")
 
 # test set - the set of queues to test with. a test queue belongs to a test set
-test_set = os.environ.get('test_set')
+test_set = os.environ.get("test_set")
 
 # bool to decide if we want to keep looping through the queue,
 # which means that the first model in the queue is triggered again after the last model is tested
-test_keep_looping = os.environ.get('test_keep_looping')
+test_keep_looping = os.environ.get("test_keep_looping")
 
 # function to load the workspace details from test queue file
 # even model we need to test belongs to a queue. the queue name is passed as environment variable test_queue
@@ -58,13 +63,15 @@ def get_test_queue() -> ConfigBox:
     queue_file = f"../../config/queue/{test_set}/{test_queue}.json"
     with open(queue_file) as f:
         return ConfigBox(json.load(f))
+
+
 # function to load the sku override details from sku-override file
 # this is useful if you want to force a specific sku for a model
 
 
 def get_sku_override():
     try:
-        with open(f'../../config/sku-override/{test_set}.json') as json_file:
+        with open(f"../../config/sku-override/{test_set}.json") as json_file:
             return json.load(json_file)
     except Exception as e:
         print(f"::warning:: Could not find sku-override file: \n{e}")
@@ -75,32 +82,32 @@ def get_sku_override():
 # so that the next step in this job can pick it up and trigger the next model using 'gh workflow run' cli command
 def set_next_trigger_model(queue):
     logger.info("In set_next_trigger_model...")
-# file the index of test_model_name in models list queue dictionary
+    # file the index of test_model_name in models list queue dictionary
     model_list = list(queue.models)
-    #model_name_without_slash = test_model_name.replace('/', '-')
-    check_mlflow_model = test_model_name.replace('/', '-')
+    # model_name_without_slash = test_model_name.replace('/', '-')
+    check_mlflow_model = test_model_name.replace("/", "-")
     if check_mlflow_model in model_list:
         index = model_list.index(check_mlflow_model)
     else:
         index = model_list.index(test_model_name)
-    #index = model_list.index(test_model_name)
+    # index = model_list.index(test_model_name)
     logger.info(f"index of {test_model_name} in queue: {index}")
-# if index is not the last element in the list, get the next element in the list
+    # if index is not the last element in the list, get the next element in the list
     if index < len(model_list) - 1:
         next_model = model_list[index + 1]
     else:
-        if (test_keep_looping == "true"):
+        if test_keep_looping == "true":
             next_model = queue[0]
         else:
             logger.warning("::warning:: finishing the queue")
             next_model = ""
-# write the next model to github step output
-    with open(os.environ['GITHUB_OUTPUT'], 'a') as fh:
-        logger.info(f'NEXT_MODEL={next_model}')
-        print(f'NEXT_MODEL={next_model}', file=fh)
+    # write the next model to github step output
+    with open(os.environ["GITHUB_OUTPUT"], "a") as fh:
+        logger.info(f"NEXT_MODEL={next_model}")
+        print(f"NEXT_MODEL={next_model}", file=fh)
 
 
-def create_or_get_compute_target(ml_client,  compute):
+def create_or_get_compute_target(ml_client, compute):
     cpu_compute_target = compute
     try:
         compute = ml_client.compute.get(cpu_compute_target)
@@ -121,18 +128,34 @@ def run_azure_ml_job(code, command_to_run, environment, compute, environment_var
         command=command_to_run,
         environment=environment,
         compute=compute,
-        environment_variables=environment_variables
+        environment_variables=environment_variables,
     )
     return command_job
 
 
 def create_and_get_job_studio_url(command_job, workspace_ml_client):
-
-    #ml_client = mlflow.tracking.MlflowClient()
+    # ml_client = mlflow.tracking.MlflowClient()
     returned_job = workspace_ml_client.jobs.create_or_update(command_job)
     # wait for the job to complete
     workspace_ml_client.jobs.stream(returned_job.name)
     return returned_job.studio_url
+
+
+def create_endpoint(workspace_ml_client, endpoint_name):
+    try:
+        logger.info("Inside creating the endpoint method")
+        endpoint = workspace_ml_client.online_endpoints.get(name=endpoint_name)
+        return endpoint
+    except ResourceNotFoundError as e:
+        logger.warning("The endpoint do not exist and now creting new endpoint")
+        endpoint = ManagedOnlineEndpoint(name=endpoint_name, auth_mode="key")
+        logger.warning("update the endpoint in the workspace")
+        workspace_ml_client.online_endpoints.begin_create_or_update(endpoint).wait()
+        return endpoint
+    except Exception as e:
+        logger.error(f"Failed due to this : {e}")
+        sys.exit(1)
+
 
 if __name__ == "__main__":
     # if any of the above are not set, exit with error
@@ -143,23 +166,22 @@ if __name__ == "__main__":
     queue = get_test_queue()
     model_list = list(queue.models)
     for test_model_name in model_list:
-        
         # sku_override = get_sku_override()
         # if sku_override is None:
         #     check_override = False
-    
+
         # if test_trigger_next_model == "true":
         #     set_next_trigger_model(queue)
         # print values of all above variables
-        logger.info (f"test_subscription_id: {queue['subscription']}")
-        logger.info (f"test_resource_group: {queue['subscription']}")
-        logger.info (f"test_workspace_name: {queue['workspace']}")
-        logger.info (f"test_model_name: {test_model_name}")
-        logger.info (f"test_sku_type: {test_sku_type}")
-        logger.info (f"test_registry: queue['registry']")
-        logger.info (f"test_trigger_next_model: {test_trigger_next_model}")
-        logger.info (f"test_queue: {test_queue}")
-        logger.info (f"test_set: {test_set}")
+        logger.info(f"test_subscription_id: {queue['subscription']}")
+        logger.info(f"test_resource_group: {queue['subscription']}")
+        logger.info(f"test_workspace_name: {queue['workspace']}")
+        logger.info(f"test_model_name: {test_model_name}")
+        logger.info(f"test_sku_type: {test_sku_type}")
+        logger.info(f"test_registry: queue['registry']")
+        logger.info(f"test_trigger_next_model: {test_trigger_next_model}")
+        logger.info(f"test_queue: {test_queue}")
+        logger.info(f"test_set: {test_set}")
         logger.info(f"Here is my test model name : {test_model_name}")
         try:
             credential = DefaultAzureCredential()
@@ -175,17 +197,21 @@ if __name__ == "__main__":
                 credential=credential,
                 subscription_id=queue.subscription,
                 resource_group_name=queue.resource_group,
-                workspace_name=queue.workspace
+                workspace_name=queue.workspace,
             )
         ws = Workspace(
             subscription_id=queue.subscription,
             resource_group=queue.resource_group,
-            workspace_name=queue.workspace
+            workspace_name=queue.workspace,
         )
         mlflow.set_tracking_uri(ws.get_mlflow_tracking_uri())
         compute_target = create_or_get_compute_target(
-            workspace_ml_client, queue.compute)
-        environment_variables = {"AZUREML_ARTIFACTS_DEFAULT_TIMEOUT":600.0,"test_model_name": test_model_name}
+            workspace_ml_client, queue.compute
+        )
+        environment_variables = {
+            "AZUREML_ARTIFACTS_DEFAULT_TIMEOUT": 600.0,
+            "test_model_name": test_model_name,
+        }
         env_list = workspace_ml_client.environments.list(name=queue.environment)
         latest_version = 0
         for env in env_list:
@@ -193,20 +219,30 @@ if __name__ == "__main__":
                 latest_version = int(env.version)
         logger.info(f"Latest Environment Version: {latest_version}")
         latest_env = workspace_ml_client.environments.get(
-            name=queue.environment, version=str(latest_version))
+            name=queue.environment, version=str(latest_version)
+        )
         logger.info(f"Latest Environment : {latest_env}")
-        command_job = run_azure_ml_job(code="./", command_to_run="python generic_model_download_and_register1.py",
-                                        environment=latest_env, compute=queue.compute, environment_variables=environment_variables)
+        endpoint_name = queue.compute.lower()
+        endpoint = create_endpoint(
+            workspace_ml_client=workspace_ml_client, endpoint_name=endpoint_name
+        )
+        command_job = run_azure_ml_job(
+            code="./",
+            command_to_run="python generic_model_download_and_register1.py",
+            environment=latest_env,
+            compute=queue.compute,
+            environment_variables=environment_variables,
+        )
 
         try:
             create_and_get_job_studio_url(command_job, workspace_ml_client)
             InferenceAndDeployment = ModelInferenceAndDeployemnt(
-                test_model_name=test_model_name.lower(),
+                test_model_name=test_model_name,
                 workspace_ml_client=workspace_ml_client,
-                registry=queue.registry
+                registry=queue.registry,
             )
             InferenceAndDeployment.model_infernce_and_deployment(
-                instance_type=queue.instance_type
+                instance_type=queue.instance_type, endpoint=endpoint
             )
         except Exception as ex:
             logger.info(f"The failed model is this one: {test_model_name}")
@@ -215,4 +251,4 @@ if __name__ == "__main__":
     #      # Log the exception, you can customize this part based on your needs
     #      logger.error(f"Error processing model {model_name}: {str(e)}")
     #      # Continue to the next model
-    #continue
+    # continue
